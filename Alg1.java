8eb348b89fecebf24104f6ff21669f216b7a33cf
daniraba <daniellarabayev@gmail.com>
2025-02-11 12:46:26 -0500

    Update algorithm 1

--- Alg1.java
+++ Alg1.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
-
-//Hello vro
-
+import java.util.Random;
 
 public class Alg1 {
-    int sum, n, submax;
-    long totalTime, endTime, startTime;
+    private int sum, submax;
 
+    public int bruteForce(int[] a, int n) {
+        submax = Integer.MIN_VALUE; // Initialize submax
 
-    private class BruteForce {
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i < n; i++) {
             sum = 0;
-            for (int j = i; j <= n; j++) {
-                sum = sum + a.get(j);
+            for (int j = i; j < n; j++) {
+                sum += a[j];
                 submax = Math.max(submax, sum);
             }
         }
@@ -20,13 +18,20 @@ public class Alg1 {
     }
 
     public static void main(String[] args) {
-        long startTime = System.nanoTime();
         Scanner scan = new Scanner(System.in);
-        Integer n = scan.nextInt();
+        Random rand = new Random();
+        System.out.println("Enter the array size.");
+        int n = scan.nextInt();
         int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = rand.nextInt(21) - 10; // Generate random values between -10 and 10 for elements in array n
+        }
+        Alg1 alg = new Alg1();
+        long startTime = System.nanoTime();
+        int maxSum = alg.bruteForce(a, n);
         long endTime = System.nanoTime();
         long totalTime = endTime - startTime;
-        System.out.println("The time it took was: " + totalTime);
-
+        System.out.println("Maximum subarray sum: " + maxSum + "\nThe time it took (nanoseconds) was: " + totalTime);
+        scan.close();
     }
 }
