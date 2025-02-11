7da09eaf31e71088166a6f302abcd608aee18dde
daniraba <daniellarabayev@gmail.com>
2025-02-11 14:06:40 -0500

    Debugging

--- Alg1.java
+++ Alg1.java
@@ -4,11 +4,11 @@ import java.util.Random;
 public class Alg1 {
     private int sum, submax;
 
-    public int bruteForce(int[] a, int n) {
-        submax = Integer.MIN_VALUE; // Initialize submax
+    public static int bruteForce(int[] a, int n) {
+        int submax = Integer.MIN_VALUE; // Initialize submax
 
         for (int i = 1; i < n; i++) {
-            sum = 0;
+            int sum = 0;
             for (int j = i; j < n; j++) {
                 sum += a[j];
                 submax = Math.max(submax, sum);
@@ -17,35 +17,41 @@ public class Alg1 {
         return submax;
     }
 
-    public int divideConquer(int[] a, int n) {
-        submax(int[] a, int low, int high) {
-            if (high == low) {
-                return a[low];
-            }
-            else {
-                mid = Math.floorDiv(low + high, 2);
-                leftsum = submax(a, low, mid);
-                rightsum = submax(a, mid + 1, high);
-                crosssum = crossingsub(a, low, mid, high);
-            }
+    public static int divideConquer(int[] a, int low, int high) {
+        if (low == high) {
+            return a[low];
         }
+        
+        int mid = Math.floorDiv(low + high, 2);
+        int leftSum = divideConquer(a, low, mid);
+        int rightSum = divideConquer(a, mid + 1, high);
+        int crossSum = crossingSub(a, low, mid, high);
 
-        crossingsub(int[] a, low, mid, high) {
-            for (int i = mid; i < low; i++) {
-                sum += a[i];
-                if (sum > leftsum) {
-                    leftsum == sum;
-                }
-            }
-            for (int j = mid + 1; j < high; j++) {
-                sum += a[j];
-                if (sum > rightsum) {
-                    rightsum == sum;
-                }
-            }
-            return leftsum + rightsum;
-        }
+        return Math.max(Math.max(leftSum, rightSum), crossSum);
     }
+
+    
+
+    public static int crossingSub(int[] a, int low, int mid, int high) {
+        int leftSum = Integer.MIN_VALUE, sum = 0; 
+        for (int i = mid; i >= low; i--) {
+            sum += a[i];
+            if (sum > leftSum) {
+                leftSum = sum;
+            }
+        }
+        int rightSum = Integer.MIN_VALUE;
+        sum = 0;
+        for (int j = mid + 1; j <= high; j++) {
+            sum += a[j];
+            if (sum > rightSum) {
+                rightSum = sum;
+            }
+        }
+        return leftSum + rightSum;
+    }
+
+    
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -57,16 +63,20 @@ public class Alg1 {
             a[i] = rand.nextInt(21) - 10; // Generate random values between -10 and 10 for elements in array n
         }
         Alg1 alg = new Alg1();
+
+        //Brute force
         long startTime = System.nanoTime();
         int maxSum = alg.bruteForce(a, n);
         long endTime = System.nanoTime();
         long totalTime = endTime - startTime;
         System.out.println("Brute Force Algorithm\n-----------\nMaximum subarray sum: " + maxSum + "\nThe time it took (nanoseconds) was: " + totalTime);
+        
+        //Divide and conquer
         long startTime2 = System.nanoTime();
-        int maxSum2 = alg.divideConquer(a, n);
+        int maxSum2 = alg.divideConquer(a, 0, n-1);
         long endTime2 = System.nanoTime();
         long totalTime2 = endTime2 - startTime2;
-        System.out.println("Divide and Conquer Algorithm\n-----------\nMaximum subarray sum: " + maxSum + "\nThe time it took (nanoseconds) was: " + totalTime);
+        System.out.println("Divide and Conquer Algorithm\n-----------\nMaximum subarray sum: " + maxSum2 + "\nThe time it took (nanoseconds) was: " + totalTime);
         scan.close();
     }
 }
